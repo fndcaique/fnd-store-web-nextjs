970f4397723a58c7e90f94114aed7e44271412e1
Fernando Soares <fndcaique@gmail.com>
2022-06-17 07:38:43 -0300

    add link to products on home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -6,6 +7,16 @@ export default function Home() {
       <Head>
         <title>Store</title>
       </Head>
+
+      <nav>
+        <ul>
+          <li>
+            <Link href='/products'>
+              <a>Products</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
 
       <main>
         <section>
