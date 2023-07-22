c8f3b0988d24b7b4d689828d5219d51051393611
Fernando Soares <fndcaique@gmail.com>
2023-07-22 15:40:42 -0300

    fix: lint staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,11 @@
 // lint-staged.config.js
+// eslint-disable-next-line no-undef
 module.exports = {
   // Type check TypeScript files
   '**/*.(ts|tsx)': () => 'pnpm tsc --noEmit',
 
   // Lint then format TypeScript and JavaScript files
   '**/*.(ts|tsx|js|jsx)': (filenames) => [
-    `pnpm eslint --fix ${filenames.join(' ')}`,
-  ],
+    `pnpm eslint --fix ${filenames.join(' ')}`
+  ]
 };
